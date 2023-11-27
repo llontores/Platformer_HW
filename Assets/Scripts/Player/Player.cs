d50e72cb98bdaf79d77fc000b2fca8fc10defb8a
llontores <80592083+llontores@users.noreply.github.com>
2023-11-27 09:44:20 +0600

    1

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -10,14 +10,20 @@ public class Player : MonoBehaviour
     [SerializeField] private int _health;
     [SerializeField] private float _speed;
     [SerializeField] private float _jumpForce;
+    [SerializeField] private Transform _attackPoint;
+    [SerializeField] private float _attackRange;
+    [SerializeField] private LayerMask _enemiesMask;
+    [SerializeField] private int _damage;
 
     private Rigidbody2D _rigidbody2D;
     private Animator _animator;
     private float _horizontalInput;
+    private bool _isGrounded;
     private float _sign;
 
     private void Start()
     {
+        _isGrounded = true;
         _animator = GetComponent<Animator>();
         _rigidbody2D = GetComponent<Rigidbody2D>();
     }
@@ -33,18 +39,52 @@ public class Player : MonoBehaviour
 
         if (Mathf.Abs(_horizontalInput) > 0.01)
         {
-            _animator.SetBool("IsWalking", true);
+            _animator.SetFloat("Speed", Mathf.Abs(_horizontalInput));
             float sign = Mathf.Sign(_horizontalInput);
             transform.localScale = new Vector3(sign * Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
         }
 
-        _animator.SetBool("IsWalking", false);
-
         _rigidbody2D.velocity = new Vector2(_speed * _horizontalInput, _rigidbody2D.velocity.y);
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+        {
+            _isGrounded = false;
+            _animator.SetBool("IsJumping", true);
             _rigidbody2D.AddForce(_jumpForce * Vector2.up, ForceMode2D.Impulse);
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            Attack();
+        }
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.TryGetComponent(out Ground ground))
+        {
+            _animator.SetBool("IsJumping", false);
+            _isGrounded = true;
+        }
+    }
 
+    private void Attack()
+    {
+        _animator.SetTrigger("Attack");
+
+        Collider2D[] enemies = Physics2D.OverlapCircleAll(_attackPoint.position, _attackRange, _enemiesMask);
+
+        foreach (Collider2D enemy in enemies)
+        {
+            enemy.GetComponent<Enemy>().TakeDamage(_damage);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if(_attackPoint == null)
+            return;
+
+        Gizmos.DrawWireSphere(_attackPoint.position, _attackRange);
+    }
 }
